a8f83d9139cdf14f1ca8d9f97cf116fe2ab2b8bb
loren <s281325@studenti.polito.it>
2023-04-26 11:40:40 +0200

    Risoluzione non ottimizzata mediante uso di LinkedList

--- src/main/java/it/polito/tdp/poweroutages/model/Model.java
+++ src/main/java/it/polito/tdp/poweroutages/model/Model.java
@@ -3,6 +3,7 @@ package it.polito.tdp.poweroutages.model;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.AbstractCollection;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -46,7 +47,6 @@ public class Model {
 				result.add(po);
 			}
 		}
-		
 		return result;
 	}
 	
@@ -55,20 +55,20 @@ public class Model {
 		return (double) duration.toSeconds()/31556952;
 	}
 
-	public Double totaleOreDisservizio(List<PowerOutage> listaDisservizi) {
+	public Double totaleOreDisservizio(LinkedList<PowerOutage> listaDisservizi) {
 		double totale = 0;
 		for (PowerOutage po : listaDisservizi) {
-			totale = totale + po.getDurataDisservizioOre();	
+			totale = totale + po.getDurataDisservizioOre();
 		}
 		return totale;
 	}
 	
-	public Double anniCoperti(List<PowerOutage> listaDisservizi) {
+	public Double anniCoperti(LinkedList<PowerOutage> listaDisservizi) {
 		LocalDateTime minAssoluto = listaDisservizi.get(0).getDateEventBegan();
 		LocalDateTime maxAssoluto = listaDisservizi.get(0).getDateEventFinished();
-		for (int i = 1; i < listaDisservizi.size(); i++) {
-			LocalDateTime minLocale = listaDisservizi.get(i).getDateEventBegan();
-			LocalDateTime maxLocale = listaDisservizi.get(i).getDateEventFinished();
+		for (PowerOutage po : listaDisservizi) {
+			LocalDateTime minLocale = po.getDateEventBegan();
+			LocalDateTime maxLocale = po.getDateEventFinished();
 			if (minLocale.isBefore(minAssoluto)) {
 				minAssoluto = minLocale;
 			}
@@ -93,7 +93,6 @@ public class Model {
 		LinkedList<PowerOutage> tuttiDisserviziNerc = this.filtroNerc(nerc, this.powerOutageIdMap);
 		
 		for (PowerOutage po : tuttiDisserviziNerc) {
-			
 			if (po.getDurataDisservizioOre() <= maxOreDisservizio) {
 				soluzioneParziale.add(po);
 				ricerca(tuttiDisserviziNerc, soluzioneParziale, 1, durataPolizza, maxOreDisservizio);
